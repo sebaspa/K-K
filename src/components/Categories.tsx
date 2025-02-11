9ed149d1691e2116f7b5e32acef90e8b384f2ed6
Juan Sebastian Parra M <sebaspa0107@gmail.com>
2025-02-11 15:20:26 -0500

    Correcion de version mobile

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,29 +8,31 @@ const Categories = () => {
     <div className="py-12">
       <div className="container mx-auto px-4 max-w-7xl">
         <h4 className="text-brick-red-700 text-center text-2xl mb-5 font-medium">Find the ideal insurance for you</h4>
-        <div className="grid grid-cols-3 gap-12 max-w-4xl mx-auto">
-          {
-            <>
-              <CardCategory
-                title="Travel"
-                icon={plane}
-                list={["Bullet one", "Bullet two", "Bullet three"]}
-                button={{ text: 'View Details', url: '#' }}
-              />
-              <CardCategory
-                title="Life & Health"
-                icon={hearth}
-                list={["Bullet one", "Bullet two", "Bullet three"]}
-                button={{ text: 'View Details', url: '#' }}
-              />
-              <CardCategory
-                title="Cars & Transport"
-                icon={car}
-                list={["Bullet one", "Bullet two", "Bullet three"]}
-                button={{ text: 'View Details', url: '#' }}
-              />
-            </>
-          }
+        <div className="w-full lg:w-4xl overflow-x-auto mx-auto">
+          <div className="flex gap-8 md:gap-12">
+            {
+              <>
+                <CardCategory
+                  title="Travel"
+                  icon={plane}
+                  list={["Bullet one", "Bullet two", "Bullet three"]}
+                  button={{ text: 'View Details', url: '#' }}
+                />
+                <CardCategory
+                  title="Life & Health"
+                  icon={hearth}
+                  list={["Bullet one", "Bullet two", "Bullet three"]}
+                  button={{ text: 'View Details', url: '#' }}
+                />
+                <CardCategory
+                  title="Cars & Transport"
+                  icon={car}
+                  list={["Bullet one", "Bullet two", "Bullet three"]}
+                  button={{ text: 'View Details', url: '#' }}
+                />
+              </>
+            }
+          </div>
         </div>
       </div>
     </div>
--- src/components/cards/CardCategory.tsx
+++ src/components/cards/CardCategory.tsx
@@ -14,12 +14,12 @@ interface CardCategoryProps {
 
 const CardCategory = ({ title, icon, list, button }: CardCategoryProps) => {
   return (
-    <div className="col-span-1">
+    <div className="col-span-1 w-full min-w-[180px]">
       <div className="card-box-shadow py-6 rounded-md mb-2 px-4">
-        <div className="bg-brick-red-700 w-[140px] h-[140px] rounded-full flex items-center justify-center mx-auto mb-2">
-          <img src={icon} alt={title} />
+        <div className="bg-brick-red-700 w-[90px] h-[90px] md:w-[140px] md:h-[140px] rounded-full flex items-center justify-center mx-auto mb-2">
+          <img src={icon} alt={title} className="w-10 h-10 md:w-16 md:h-16" />
         </div>
-        <p className="font-medium text-xl text-boulder-900 text-center mb-6">{title}</p>
+        <p className="font-medium text-lg md:text-xl text-boulder-900 text-center mb-4 md:mb-6">{title}</p>
         <div className="mx-auto size-fit">
           <ul>
             {
