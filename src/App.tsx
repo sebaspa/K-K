693f30fc2da40d656443ece2007eaabc827be080
Juan Sebastian Parra M <sebaspa0107@gmail.com>
2025-02-11 22:29:00 -0500

    Menu principal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ContentForm from "./components/ContentForm"
 import CustomersSaying from "./components/CustomersSaying"
 import Footer from "./components/Footer"
 import MainBanner from "./components/MainBanner"
+import MainMenu from "./components/MainMenu"
 import OurServices from "./components/OurServices"
 import Recomendation from "./components/Recomendation"
 import SearchCategories from "./components/SearchCategories"
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <>
+      <MainMenu />
       <MainBanner />
       <Recomendation />
       <Categories />
