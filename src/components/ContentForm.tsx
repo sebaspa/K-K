e250c97864e8bab4fe7ad2056aef15af5cccfb99
Juan Sebastian Parra M <sebaspa0107@gmail.com>
2025-02-11 22:00:45 -0500

    Correccion mobile de formulario

--- src/components/ContentForm.tsx
+++ src/components/ContentForm.tsx
@@ -4,10 +4,12 @@ const ContentForm = () => {
   return (
     <div className="pb-20">
       <div className="container mx-auto max-w-6xl -mt-[320px]">
-        <div className="flex gap-12 bg-white">
-          <img src="/src/assets/vertical-family.jpg" />
-          <div className="w-full pt-10 pb-4 px-12 bg-white">
-            <h3 className="font-bold text-brick-red-700 text-2xl text-center mb-10">Book Appointment</h3>
+        <div className="flex flex-col md:flex-row gap-12 bg-white">
+          <img src="/src/assets/vertical-family.jpg" className="hidden md:block" />
+          <img src="/src/assets/horizontal-family.jpg" className="block md:hidden" />
+          <h3 className="font-bold text-brick-red-700 text-4xl px-6 block md:hidden">Get A Free Quote Here</h3>
+          <div className="w-full pt-0 md:pt-10 pb-4 px-6 lg:px-12 bg-white">
+            <h3 className="font-bold text-brick-red-700 text-2xl text-center mb-10 hidden md:block">Book Appointment</h3>
             <ContactForm />
           </div>
         </div>
